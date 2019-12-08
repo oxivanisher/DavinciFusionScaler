23af080b3b92aca3d93c964b35766a6a84a11afb
Marc Urben <aegnor@mittelerde.ch>
2019-12-08 14:18:53 +0100

    seems to be working

--- davinci_scaler.py
+++ davinci_scaler.py
@@ -3,15 +3,27 @@
 
 import click
 import logging
+import os
+
+level = logging.INFO
+if os.environ.get("DEBUG", "true").lower() == "true":
+    level = logging.DEBUG
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-7s %(message)s",
+    datefmt="%Y-%d-%m %H:%M:%S",
+    level=level,
+)
 
 @click.command()
 @click.option('--input_file', prompt='Input filename', help='The settings file to be read')
 @click.option('--output_file', prompt='Output filename', help='The settings file to be written')
-@click.option('--multiplier', prompt='Multiplier', help='The multiplier to be applied to the .settings file')
+@click.option('--multiplier', prompt='Multiplier', help='The multiplier to be applied to the .settings file',
+              type=float)
 def scale(input_file, output_file, multiplier):
     """This program scales keyframes in Davinci Resolve .settings files."""
 
-    loggin.info("Loading file %s" % input_file)
+    logging.info("Loading file %s" % input_file)
     with open(input_file) as f:
         content = f.readlines()
     # remove whitespaces at the end of the line (\n)
@@ -28,15 +40,11 @@ def scale(input_file, output_file, multiplier):
             logging.info("Found keyframe start line in %s" % line_number)
             keyframes_line_found = True
             indentation = len(line) - len(line.lstrip())
-            output.append(line)
-            continue
-        if line == "%s}" % "\t" * indentation:
+        elif line == "%s}" % ("\t" * indentation):
             logging.info("Found keyframe end line in %s" % line_number)
             keyframes_line_found = False
             indentation = 0
-            output.append(line)
-            continue
-        if keyframes_line_found:
+        elif keyframes_line_found:
             indentation_string = "\t" * (indentation + 1)
             clean_line = line[len(indentation_string):]
 
@@ -47,11 +55,11 @@ def scale(input_file, output_file, multiplier):
             if not clean_line.startswith('['):
                 logging.error("Line %s not starting with a KeyFrame definition [. Something is fishy!" % line_number)
                 raise
-            keyframe = float(line[2:(line.find(']') - 1)])
-            converted_keyframe = keyframe * multiplier
+            keyframe = line[(indentation + 2):line.find(']')]
+            converted_keyframe = float(keyframe) * multiplier
             converted_keyframes += 1
-            line.replace(keyframe, converted_keyframe, 1)
-            logging.info("Converted keyframe %s to %s" % (keyframe, converted_keyframe))
+            logging.info("Converted keyframe %s to %s" % (str(keyframe), str(converted_keyframe).replace('.0', '')))
+            line = line.replace("[%s]" % keyframe, "[%s]" % str(converted_keyframe).replace('.0', ''), 1)
 
         output.append(line)
 
